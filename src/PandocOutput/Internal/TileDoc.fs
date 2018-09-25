baf5a033c1a6587e5e4ea3f5710ebbcb2594ab27
stephentetley <stephen.tetley@gmail.com>
2018-09-25 10:33:59 +0100

    Work on _textual_ combinators for Markdown. Most are now implemented. Tile combinators for lists, tables, etc. need working out and implementing.

--- src/PandocOutput/Internal/TileDoc.fs
+++ src/PandocOutput/Internal/TileDoc.fs
@@ -8,51 +8,134 @@ open System.Text
 open PandocOutput.Internal.Common
 
 /// This is not a "pretty printer" as it makes no effort to "fit" the output.
-type HDoc = 
+type Text = 
     | Empty
-    | HText of string
-    | HDoc of HDoc * HDoc
-    static member (+) (a:HDoc, b:HDoc) = HDoc(a,b)
+    | Raw of string
+    | Horizontal of Text * Text
+    static member (+) (a:Text, b:Text) = Horizontal(a,b)
 
-let renderHDoc (width:int) (doc:HDoc) : string list = 
+let renderText (width:int) (doc:Text) : string list = 
     let sb = new StringBuilder ()
-    let rec work (doc:HDoc) (cont : unit -> 'a) = 
+    let rec work (doc:Text) (cont : unit -> 'a) = 
         match doc with
         | Empty -> cont ()
-        | HText str -> sb.Append(str) |> ignore; cont ()
-        | HDoc(d1,d2) -> 
+        | Raw str -> sb.Append(str) |> ignore; cont ()
+        | Horizontal(d1,d2) -> 
             work d1 (fun _ ->
             work d2 (fun _ -> 
             cont ()))
     work doc (fun _ -> ()) 
     sb.ToString () |> breaklines width
 
-let character (ch:char) : HDoc = HText (ch.ToString())
-let rawtext (text:string) : HDoc = HText text
-        
-let enclose (left:HDoc) (right:HDoc) (d1:HDoc) : HDoc = 
-    left + d1 + right
+let empty : Text = Empty
 
-/// A single space
-let space : HDoc = character ' '
+let character (ch:char) : Text = Raw <| ch.ToString()
 
-let dot : HDoc = character '.'
-let semi : HDoc = character ';'
-let colon : HDoc = character ':'
-let comma : HDoc = character ','
-let backslash : HDoc = character '\\'
-let forwardslash : HDoc = character '/'
+/// TODO - should probably also have a version that does escaping...
+let rawtext (text:string) : Text = Raw text
 
-let underscore : HDoc = character '_'
 
 
 // Note (@) appears to only operate on lists (it doesn't work for array...).
-// This implies we should use another 
+// This implies we should use another combinator symbol for joining things.
 
-// Prefix2 is for e.g list items where the first prefix is * and the subsequent ones are indents.
-// Can this representation accommodate tables?
-type VDoc = 
-    | VDoc of string list
-    | Prefix of string * VDoc
-    | Suffix of VDoc * string
-    | Prefix2 of string * string * VDoc
+
+/// Horizontal concat with a separating space 
+let (<+>) (d1:Text) (d2:Text) : Text = 
+   d1 + character ' ' + d2
+
+let bang : Text = character '!'
+let colon : Text = character ':'
+let space : Text = character ' '
+
+let enclose (left:Text) (right:Text) (d1:Text) : Text = 
+    left + d1 + right
+
+
+let parens (source:Text) : Text = 
+    enclose (character '(') (character ')') source
+
+let squareBrackets (source:Text) : Text = 
+    enclose (character '[') (character ']') source
+
+/// Can be used for inlining links.
+let angleBrackets (source:Text) : Text = 
+    enclose (character '<') (character '>') source
+
+let singleQuotes (source:Text) : Text = 
+    enclose (character '\'') (character '\'') source
+
+let doubleQuotes (source:Text) : Text = 
+    enclose (character '"') (character '"') source
+
+/// Emphasis
+let asterisks (source:Text) : Text = 
+    enclose (character '*') (character '*') source
+
+/// Emphasis
+let underscores (source:Text) : Text = 
+    enclose (character '_') (character '_') source
+
+/// Strong emphasis
+let doubleAsterisks (source:Text) : Text = 
+    enclose (rawtext "**") (rawtext "**") source
+
+/// Strong emphasis
+let doubleUnderscores (source:Text) : Text = 
+    enclose (rawtext "__") (rawtext "__") source
+
+/// Backticks for inline code.
+let backticks (source:Text) : Text = 
+    enclose (character '`') (character '`') source
+
+/// Backticks for inline code.
+let doubleBackticks (source:Text) : Text = 
+    enclose (rawtext "``") (rawtext "``") source
+
+/// [A link](/path/to)
+///
+/// [A link](/path/to "Title") 
+let inlineLink (altText:Text) (path:string) (title:option<string>) : Text = 
+    let title1  = 
+        match title with
+        | None -> empty
+        | Some ss -> space + doubleQuotes (rawtext ss)
+    (squareBrackets altText) + parens (rawtext path + title1)
+
+
+let defLinkReference (identifier:string) (path:string) (title:option<string>) : Text = 
+    let title1  = 
+        match title with
+        | None -> empty
+        | Some ss -> space + doubleQuotes (rawtext ss)
+    squareBrackets (rawtext identifier) + colon <+> angleBrackets (rawtext path) + title1
+
+let useLinkReference (altText:Text) (identifier:string) : Text = 
+    squareBrackets altText + squareBrackets (rawtext identifier)
+
+let inlineImage (altText:Text) (path:string) (title:option<string>) : Text = 
+    let title1  = 
+        match title with
+        | None -> empty
+        | Some ss -> space + doubleQuotes (rawtext ss)
+    bang + (squareBrackets altText) + parens (rawtext path + title1)
+
+let defImageReference (identifier:string) (path:string) (title:option<string>) : Text = 
+    let title1  = 
+        match title with
+        | None -> empty
+        | Some str -> space + doubleQuotes (rawtext str)
+    squareBrackets (rawtext identifier) + colon <+> rawtext path + title1
+
+let useImageReference (altText:Text) (identifier:string) : Text = 
+    bang + (squareBrackets altText) + (squareBrackets <| rawtext identifier)
+
+
+
+//// Prefix2 is for e.g list items where the first prefix is * and the subsequent ones are indents.
+//// Can this representation accommodate tables?
+//type Tile = 
+//    | VDoc of string list
+//    | Prefix of string * Tile
+//    | Suffix of Tile * string
+//    | Prefix2 of string * string * Tile
--- src/TestTileDoc.fsx
+++ src/TestTileDoc.fsx
@@ -9,23 +9,8 @@ open PandocOutput.Internal.TileDoc
 
 
 
-/// Emphasis
-let asterisks (source:HDoc) : HDoc = 
-    enclose (character '*') (character '*') source
-
-/// Emphasis
-let underscores (source:HDoc) : HDoc = 
-    enclose (character '_') (character '_') source
-
-/// Strong emphasis
-let doubleAsterisks (source:HDoc) : HDoc = 
-    enclose (rawtext "**") (rawtext "**") source
-
-/// Strong emphasis
-let doubleUnderscores (source:HDoc) : HDoc = 
-    enclose (rawtext "__") (rawtext "__") source
 
 let test01 () = 
-    doubleUnderscores (rawtext "Hollow" + space + rawtext "world!") 
-        |> renderHDoc 40 
+    doubleUnderscores (rawtext "Hollow" <+> rawtext "world!") 
+        |> renderText 40 
 
